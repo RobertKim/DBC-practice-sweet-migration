f3a93fc41104aa03873a671a7cacee841c0069f9
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-10 20:10:49 -0700

    Removed default index public/index.html
    Added user_helper
    app/helpers/users_helper.rb

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,18 @@
+module UsersHelper 
+  # def login
+  #   if session[:user_id]
+  #     return @current_user ||= User.find(session[:user_id])
+  #   end
+  #   false
+  # end
+
+  # def gravatar_for(user, size = 100)
+  #   if user.email
+  #     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  #   else
+  #     gravatar_url = "user.png"
+  #   end
+  #   image_tag(gravatar_url, alt: user.username, class: "gravatar")
+  # end
+end
